110664e2da520c5843ed6934412020fb56728eae
Anwar <anwarqarout@gmail.com>
2021-07-11 12:33:57 +0300

    final alteration

--- tree_classes.py
+++ tree_classes.py
@@ -1,34 +1,43 @@
 class BinTreeNode:
     def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+        if isinstance(value,int):
+            self.value = value
+            self.left = None
+            self.right = None
+        else:
+            raise TypeError("Entered non integer value.")
 
     def insert(self, value):
-        if value > self.value:
-            if self.right is None:
-                self.right = BinTreeNode(value)
-            else:
-                self.right.insert(value)
+        if not isinstance(value,int):
+            raise TypeError("Entered non integer value")
+        else:
+            if value > self.value:
+                if self.right is None:
+                    self.right = BinTreeNode(value)
+                else:
+                    self.right.insert(value)
 
-        elif value < self.value:
-            if self.left is None:
-                self.left = BinTreeNode(value)
-            else:
-                self.left.insert(value)
+            elif value < self.value:
+                if self.left is None:
+                    self.left = BinTreeNode(value)
+                else:
+                    self.left.insert(value)
 
     def search(self, value):
-        if self is None:
-            return None
+        if not isinstance(value,int):
+            raise TypeError("Entered non integer value")
+        else:
+            if self is None:
+                return None
 
-        elif self.value == value:
-            return self
+            elif self.value == value:
+                return self
 
-        elif value < self.value:
-            return BinTreeNode.search(self.left, value)
+            elif value < self.value:
+                return BinTreeNode.search(self.left, value)
 
-        elif value > self.value:
-            return BinTreeNode.search(self.right, value)
+            elif value > self.value:
+                return BinTreeNode.search(self.right, value)
 
 
     def tree_returner_helper(self):
@@ -54,17 +63,26 @@ class BinTree:
         self.root = None
 
     def insert(self, value):
-        if self.root is None:
-            self.root = BinTreeNode(value)
+        if not isinstance(value, int):
+            raise TypeError("Entered non integer value")
         else:
-            self.root.insert(value)
+            if self.root is None:
+                self.root = BinTreeNode(value)
+            else:
+                self.root.insert(value)
 
     def search(self, value):
-        if self.root is None:
-            print("Tree is empty")
+        if not isinstance(value, int):
+            raise TypeError("Entered non integer value")
         else:
-            node = self.root.search(value)
-            if node is None:
-                print("Not found")
+            if self.root is None:
+                print("tree is empty")
+                return "tree is empty"
             else:
-                print("Node found! value = ", node.value)
+                node = self.root.search(value)
+                if node is None:
+                    print("Node not found!")
+                    return "Node not found."
+                else:
+                    print("Node found! value = ", node.value)
+                    return "Node found! value =",node.value
